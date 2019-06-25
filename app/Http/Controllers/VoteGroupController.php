c5637954120aa927b28e8e00da27e7c9906347f1
Ye Shu <chshu@protonmail.ch>
2019-06-25 13:53:07 +0800

    Add Models: Vote, VoteGroup, and VoteCriteria Add models and corresponding migration, factory, and resource controllers

---
+++ app/Http/Controllers/VoteGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\VoteGroup;
+use Illuminate\Http\Request;
+
+class VoteGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\VoteGroup  $voteGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VoteGroup $voteGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\VoteGroup  $voteGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VoteGroup $voteGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\VoteGroup  $voteGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VoteGroup $voteGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\VoteGroup  $voteGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VoteGroup $voteGroup)
+    {
+        //
+    }
+}
---
+++ database/factories/VoteCriteriaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\VoteCriteria;
+use Faker\Generator as Faker;
+
+$factory->define(VoteCriteria::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
